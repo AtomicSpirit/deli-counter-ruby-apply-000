efb2ace81b86dcacdae6fdced670bcd5982a6c95
Daniel Park <park.daniel1@gmail.com>
2020-04-04 01:23:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,31 +5,28 @@ if katz_deli == []
 puts "The line is currently empty."
 else
 new_line = []
-numbers =  1
-katz_deli.each  {|names| new_line << " #{numbers}. " << "#{names.strip}"
-numbers += 1
-}
+katz_deli.each_with_index  do|names, index|
+  new_line << " #{index + 1}. #{names}"
+end
 puts "The line is currently:#{new_line.join}"
+# numbers = 1
+# katz_deli.map {|names| katz_deli << "#{numbers}. " <<  "#{names}"
+# numbers += 1
+# }
+# puts "The line is currently:#{katz_deli.join}"
 end
 end
-
-
-
-
-
 
 
 
 def take_a_number(katz_deli, name)
 katz_deli << name
-new_line = []
-numbers =  1
-while numbers <= katz_deli.length
-katz_deli.each {|names| new_line << "#{numbers}" << "#{names}"
-numbers += 1
-}
+# numbers =  1
+# while numbers <= katz_deli.length
+# katz_deli.map {|names| katz_deli << "#{numbers}"  "#{names}"
+# numbers += 1
+# }
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
 end
 
 
